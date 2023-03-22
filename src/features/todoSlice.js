e0f28735050b557319774c53b7f95d54043da707
pankaj-kb <pankajb2601@gmail.com>
2023-03-22 22:27:00 +0530

    reducer functionality added

--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -1,1 +1,29 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [], //Storing todos as in array
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        title: action.payload,
+        completed: false,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    editTodo: () => {},
+    completeTodo: () => {},
+  },
+});
+
+export const { addTodo, removeTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
