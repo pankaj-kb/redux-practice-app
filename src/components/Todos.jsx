a758167ea02ab128fec3808f09dbc41fc487a9c6
pankaj-kb <pankajb2601@gmail.com>
2023-03-24 22:12:24 +0530

    added tooltip

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { useDispatch } from "react-redux";
 import { removeTodo, addTodo } from "../features/todoSlice";
+import "react-tippy/dist/tippy.css";
+import { Tooltip } from "react-tippy";
 
 function Todos() {
   const todos = useSelector((state) => state.todos.todos);
@@ -20,23 +22,42 @@ function Todos() {
     }
   };
 
+  // const markComplete = () => {
+  //   if ()
+  // }
+
   return (
     <div>
       {todos.map((todo) => (
         <div key={todo.id}>
           <h1>{todo.title}</h1>
-          <button onClick={() => dispatch(removeTodo(todo.id))}>
-            Remove Task
-          </button>
+          <Tooltip
+            title="Remove task from list"
+            position="bottom"
+            trigger="mouseenter"
+          >
+            <button onClick={() => dispatch(removeTodo(todo.id))}>
+              Remove Task
+            </button>
+          </Tooltip>
+          <Tooltip
+            title="Mark the task as complete"
+            position="bottom"
+            trigger="mouseenter"
+          >
+            <button onClick={() => dispatch(removeTodo(todo.id))}>Done</button>
+          </Tooltip>
         </div>
       ))}
       <div>
-        <input
-          type="text"
-          placeholder="Enter Task"
-          value={inputTask}
-          onChange={handleInputTask}
-        />
+        <Tooltip title="Enter task here" position="bottom" trigger="mouseenter">
+          <input
+            type="text"
+            placeholder="Enter Task"
+            value={inputTask}
+            onChange={handleInputTask}
+          />
+        </Tooltip>
         <button onClick={handleAddTodo}>Add Task</button>
       </div>
     </div>
